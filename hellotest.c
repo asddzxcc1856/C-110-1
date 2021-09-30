cf98fc46d01f675bf4025eb513d22a95b0f382c1
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-09-30 13:53:17 +0800

    test add program to github

---
+++ hellotest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	int a = 100;
+	
+	for (int i = 0 ; i < a ; i++)
+	{
+		printf("hello, world!!!");
+	}
+	
+    system("pause");
+	return 0;
+}
+
+
